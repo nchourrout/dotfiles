6c0345f813e5241991a1d438ef31fe1281cf6599
Nicolas Chourrout <nchourrout@gmail.com>
2015-01-10 19:26:14 +0000

    No need to sudo here

--- osx/brew.sh
+++ osx/brew.sh
@@ -1,33 +1,28 @@
 #!/usr/bin/env bash
- 
-# Ask for the administrator password upfront.
-sudo -v
- 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+source ../utils/utils.sh
 
 brew update
 brew upgrade
- 
+
 # Install GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
- 
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
- 
+
 # Install Bash 4
 brew install bash
- 
+
 # Install more recent versions of some OS X tools
 brew tap homebrew/dupes
 brew install homebrew/dupes/grep
- 
+
 # Install handbrakecli
 brew tap sceaga/tap
 brew install handbrakecli
- 
+
 $PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
- 
+
 binaries=(
   ack
   duff
@@ -43,13 +38,13 @@ binaries=(
   vim
   wget
 )
- 
+
 brew install ${binaries[@]}
- 
+
 brew install caskroom/cask/brew-cask
- 
+
 brew cleanup
- 
+
 apps=(
   alfred
   brackets
@@ -74,11 +69,11 @@ apps=(
   sublime-text-3
   vlc
 )
- 
+
 brew cask install --appdir="/Applications" ${apps[@]}
- 
+
 brew cask alfred link
- 
+
 brew cask cleanup
 
 start_if_needed Day-O
